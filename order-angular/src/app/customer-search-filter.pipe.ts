8dfcb9cc1fd2368a13d8c2f417fa764ac4793e36
RobSwitch <robin.schouten.be@gmail.com>
2023-05-05 15:22:17 +0200

    pipe customer search filter added

---
+++ order-angular/src/app/customer-search-filter.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {Customer} from "./model/Customer";
+
+@Pipe({
+  name: 'customerSearchFilter'
+})
+export class CustomerSearchFilterPipe implements PipeTransform {
+
+  transform(customers: Customer[], searchText: string): any[] {
+    if (searchText === undefined) {
+      return customers
+    }
+    return customers.filter(customers => customers.lastname.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()))
+  }
+
+}
